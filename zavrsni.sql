cdaf1d71f975a30f1e7998938d494f703be9e23d
Branimir-Fekete <branimir.fekete@gmail.com>
2023-05-23 15:12:06 +0200

    Update zavrsni.sql

--- zavrsni.sql
+++ zavrsni.sql
@@ -2,43 +2,47 @@ drop database if exists opgbranimirfekete;
 create database opgbranimirfekete;
 use opgbranimirfekete;
 
-create table vlasnik(
+create table kupac(
     id int not null primary key auto_increment,
-    ime varchar(50) not null,
-    prezime varchar(50) not null,
-    adresa varchar(100) not null,
-    email varchar(100),
-    telefon varchar(50),
-    oib char(11) not null,
-    iban varchar(50) not null
+    nazivSubjekta varchar(50),
+    adresa varchar(75),
+    OIB char(11),
+    IBAN varchar(50)
 );
 
-create table usluge(
-    id int primary key auto_increment,
+create table usluga(
+    id int not null primary key auto_increment,
     naziv varchar(50) not null,
     cijena decimal(10,2) not null,
-    opis text,
-    vlasnikID int,
-    foreign key (vlasnikID) references vlasnik(id)
+    kolicina decimal(10,2) not null,
+    opis text
 );
 
-create table proizvodi(
-    id int primary key auto_increment,
+create table proizvod(
+    id int not null primary key auto_increment,
     naziv varchar(50) not null,
     cijena decimal(10,2) not null,
-    količina decimal(10,2) not null,
-    opis text,
-    vlasnikID int,
-    foreign key (vlasnikID) references vlasnik(id)
+    kolicina decimal(10,2) not null,
+    opis text
 );
 
-select * from vlasnik;
-insert into vlasnik (ime,prezime,adresa,email,telefon,oib,iban) values ('branimir','fekete','petraperica','ime@prezime',123456789,98765432101,11223344556677);
+create table racun(
+    id int not null primary key,
+    PDV int not null,
+    vrijemeIzdavanja date not null,
+    opisPlacanja text not null,
+    kupac_id int not null,
+    usluga_id int not null,
+    proizvod_id int not null,
+    foreign key (usluga_id) references usluga(id),
+    foreign key (proizvod_id) references proizvod(id),
+    foreign key (kupac_id) references kupac(id)
+);
 
 
 select * from usluge;
-insert into usluge (naziv,cijena,opis) values ('oranje',999,'oranje plugom');
+insert into usluge (naziv,cijena,kolicina,opis) values ('oranje',999.99,50,'oranje plugom');
 
 
 select * from proizvodi;
-insert into proizvodi (naziv,cijena,količina,opis) values ('pšenica',888.88,2,'prodajem pšenicu');
+insert into proizvodi (naziv,cijena,količina,opis) values ('pšenica',888.88,90,'prodajem pšenicu');
