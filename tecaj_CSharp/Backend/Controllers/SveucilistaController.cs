80922eb32d28fe0d5c946bec5528c0306f3e4d2e
Branimir-Fekete <branimir.fekete@gmail.com>
2024-04-11 16:30:00 +0200

    Contorller nadogradnja

--- tecaj_CSharp/Backend/Controllers/SveucilistaController.cs
+++ tecaj_CSharp/Backend/Controllers/SveucilistaController.cs
@@ -7,62 +7,68 @@ namespace Backend.Controllers
 
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class SveucilistaController : ControllerBase
+    public class SveucilistaController : StudomatController<Sveucilista, SveucilisteDTORead, SveucilisteDTOInsertUpdate>
     {
-        private readonly StudomatContext _context;
-
-        public SveucilistaController(StudomatContext context)
+        public SveucilistaController(StudomatContext context) : base(context)
         {
-            _context = context;
+            DbSet = _context.Sveucilista;
         }
 
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return new JsonResult(_context.Sveucilista.ToList());
-        }
 
-        [HttpGet]
-        [Route("{sifra:int}")]
-        public IActionResult GetBySifra(int sifra)
-        {
-            return new JsonResult(_context.Sveucilista.Find(sifra));
-        }
+        //private readonly StudomatContext _context;
 
-        [HttpPost]
-        public IActionResult Post(Sveucilista sveucilista)
-        {
-            _context.Sveucilista.Add(sveucilista);
-            _context.SaveChanges();
-            return new JsonResult(sveucilista);
-        }
+        //public SveucilistaController(StudomatContext context)
+        //{
+        //    _context = context;
+        //}
 
-        [HttpPut]
-        [Route("{sifra:int}")]
-        public IActionResult Put(int sifra, Sveucilista sveucilista)
-        {
-            var sveucilistaIzBaze = _context.Sveucilista.Find(sifra);
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    return new JsonResult(_context.Sveucilista.ToList());
+        //}
 
-            sveucilistaIzBaze.Naziv = sveucilista.Naziv;
-            sveucilistaIzBaze.Adresa = sveucilista.Adresa;
+        //[HttpGet]
+        //[Route("{sifra:int}")]
+        //public IActionResult GetBySifra(int sifra)
+        //{
+        //    return new JsonResult(_context.Sveucilista.Find(sifra));
+        //}
 
-            _context.Sveucilista.Update(sveucilistaIzBaze);
-            _context.SaveChanges();
+        //[HttpPost]
+        //public IActionResult Post(Sveucilista sveucilista)
+        //{
+        //    _context.Sveucilista.Add(sveucilista);
+        //    _context.SaveChanges();
+        //    return new JsonResult(sveucilista);
+        //}
 
-            return new JsonResult(sveucilistaIzBaze);
-        }
+        //[HttpPut]
+        //[Route("{sifra:int}")]
+        //public IActionResult Put(int sifra, Sveucilista sveucilista)
+        //{
+        //    var sveucilistaIzBaze = _context.Sveucilista.Find(sifra);
 
-        [HttpDelete]
-        [Route("{sifra:int}")]
-        [Produces("application/json")]
-        public IActionResult Delete(int sifra)
-        {
-            var sveucilistaIzBaze = _context.Sveucilista.Find(sifra);
+        //    sveucilistaIzBaze.Naziv = sveucilista.Naziv;
+        //    sveucilistaIzBaze.Adresa = sveucilista.Adresa;
 
-            _context.Sveucilista.Remove(sveucilistaIzBaze);
-            _context.SaveChanges();
-            return new JsonResult(new { poruka = "Obrisano" });
-        }
+        //    _context.Sveucilista.Update(sveucilistaIzBaze);
+        //    _context.SaveChanges();
+
+        //    return new JsonResult(sveucilistaIzBaze);
+        //}
+
+        //[HttpDelete]
+        //[Route("{sifra:int}")]
+        //[Produces("application/json")]
+        //public IActionResult Delete(int sifra)
+        //{
+        //    var sveucilistaIzBaze = _context.Sveucilista.Find(sifra);
+
+        //    _context.Sveucilista.Remove(sveucilistaIzBaze);
+        //    _context.SaveChanges();
+        //    return new JsonResult(new { poruka = "Obrisano" });
+        //}
 
     }
 }
